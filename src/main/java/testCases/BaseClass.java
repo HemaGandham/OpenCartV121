e84988ae3bd86330b7240dafc31048c7a5a3b8cd
Hema <chittamhema@gmail.com>
2025-06-01 11:59:30 +0530

    updated pom.xml and baseclass

--- src/main/java/testCases/BaseClass.java
+++ src/main/java/testCases/BaseClass.java
@@ -3,12 +3,13 @@ package testCases;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.net.URL;
+
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Date;
 import java.util.Properties;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.openqa.selenium.OutputType;
@@ -16,13 +17,14 @@ import org.openqa.selenium.Platform;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
+
+
 import org.apache.logging.log4j.Logger;
 
 public class BaseClass {
@@ -30,24 +32,25 @@ public class BaseClass {
 	public Logger logger;// log4j2
 	public Properties p;
 
+	@SuppressWarnings("deprecation")
 	@BeforeClass(groups= {"Sanity","Regression","Master"})
 	@Parameters({ "os", "browser" })
 	void setup(String os, String br) throws IOException {
-		
+
 		//loading config.properties file
 		FileReader file=new FileReader("./src//test//resources//config.properties");
 		p=new Properties();
 		p.load(file);
-		
+
 		logger = LogManager.getLogger(this.getClass());//log4j2
-		
+
 		if(p.getProperty("execution_env").equalsIgnoreCase("remote")){
 			DesiredCapabilities cap=new DesiredCapabilities();
-			
-			//checking os 
+
+			//checking os
 			if(os.equalsIgnoreCase("windows")) {
 				cap.setPlatform(Platform.WIN11);
-				}
+			}
 			else if(os.equalsIgnoreCase("mac")){
 				cap.setPlatform(Platform.MAC);
 			}
@@ -55,19 +58,26 @@ public class BaseClass {
 				System.out.println("No matching os");
 				return;
 			}
-			
+
 			//browser
 			switch(br.toLowerCase())
 			{
-			case "chrome":cap.setBrowserName("chrome");break;
-			case "edge":cap.setBrowserName("MicrosoftEdge");break;
-			default: System.out.println("No matching browser");return;
+				case "chrome":cap.setBrowserName("chrome");break;
+				case "edge":cap.setBrowserName("MicrosoftEdge");break;
+				default: System.out.println("No matching browser");return;
 			}
-			driver=new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),cap);
+
+			WebDriverManager.chromedriver().setup(); // Make sure WebDriverManager is in your pom.xml
+			ChromeOptions options = new ChromeOptions();
+			driver =new ChromeDriver(options);
+
+
+			//driver=new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),cap);
+
 		}
-		
+
 		//local
-		if(p.getProperty("execution_env").equalsIgnoreCase("local"))
+		/*if(p.getProperty("execution_env").equalsIgnoreCase("local"))
 		{
 			switch (br.toLowerCase()) {
 			case "chrome":
@@ -84,9 +94,9 @@ public class BaseClass {
 				return;
 
 			}
-			
-		}
-		
+
+		}*/
+
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get(p.getProperty("appURL1"));//reading url from properties file
@@ -114,21 +124,21 @@ public class BaseClass {
 		String generatednumber = RandomStringUtils.randomNumeric(10);
 		return (generatedString + "@" + generatednumber);
 	}
-	
+
 	public String captureScreen(String tname) throws IOException {
-	    String timeStamp = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+		String timeStamp = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
 
-	    TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
-	    File sourceFile = takesScreenshot.getScreenshotAs(OutputType.FILE);
+		TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
+		File sourceFile = takesScreenshot.getScreenshotAs(OutputType.FILE);
 
-	    String targetFilePath = System.getProperty("user.dir") + "\\screenshots\\" + tname + "_" + timeStamp + ".png";
-	    
-	    System.out.println("path:-------   "+targetFilePath);
-	    File targetFile = new File(targetFilePath);
+		String targetFilePath = System.getProperty("user.dir") + "\\screenshots\\" + tname + "_" + timeStamp + ".png";
 
-	    sourceFile.renameTo(targetFile);
+		System.out.println("path:-------   "+targetFilePath);
+		File targetFile = new File(targetFilePath);
 
-	    return targetFilePath;
+		sourceFile.renameTo(targetFile);
+
+		return targetFilePath;
 	}
 
 	@AfterClass(groups= {"Sanity","Regression","Master"})
