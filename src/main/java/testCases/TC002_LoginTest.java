9afabfb3dabc058e647101880e6d18039def52a7
Hema <chittamhema@gmail.com>
2025-06-01 12:14:00 +0530

    updated TC002-v1

--- src/main/java/testCases/TC002_LoginTest.java
+++ src/main/java/testCases/TC002_LoginTest.java
@@ -11,7 +11,7 @@ public class TC002_LoginTest extends BaseClass{
 	
 	
 
-	@Test(groups="Sanity")
+	@Test(groups={"Sanity","Master"},priority=1)
 	public void verify_Login() {
 		
 		logger.info("****Starting the TC002_LoginTest  ****");
